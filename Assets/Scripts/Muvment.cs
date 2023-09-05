2c7d4b8c4581d249b9a5954c07aab4ed57d52570
SobolevicDaniell <sobolevdan99@yandex.ru>
2023-09-05 13:50:04 +0300

    +

--- Assets/Scripts/Muvment.cs
+++ Assets/Scripts/Muvment.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Muvment : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    
+    [SerializeField] private float forse;
+
+    [SerializeField] private Rigidbody2D _rb;
     void Start()
     {
-        
+        //_rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        UpDown();
+    }
+
+    void UpDown()
+    {
+        if (Input.GetButton("Jump") || Input.GetButton("Fire1"))
+        {
+            _rb.velocity = Vector2.up * forse;
+        }
     }
 }
