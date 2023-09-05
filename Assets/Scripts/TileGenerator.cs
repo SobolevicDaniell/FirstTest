44318d147edb66654cdb3379b0e2080434267a2d
SobolevicDaniell <sobolevdan99@yandex.ru>
2023-09-05 15:47:28 +0300

    +

---
+++ Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+  
+    
+}
